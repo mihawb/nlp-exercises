a29bd5bc14b96ba32b58ce4ce18acc957e3da8ce
mihawb <michalbanaszczak@gmail.com>
2023-08-16 23:16:16 +0200

    scraper update

---
+++ word-scraper/README.md
@@ -0,0 +1,7 @@
+# Scraper for language-by-word classifier
+Get training data consising of most common words from several languages. This dataset is necessary to run `lang_by_word_clf.ipynb` notebook.
+### Usage
+Install dependencies and download the dataset.
+```
+npm i && npm run download
+```
--- word-scraper/get_words.js
+++ word-scraper/get_words.js
@@ -8,23 +8,23 @@ const kuroshiro = new Kuroshiro()
 
 kuroshiro.init(new KuromojiAnalyzer())
 	.then(
-		() => Promise.all(getWordsList('japanese', 5000).map(async w => kuroshiro.convert(w, { to: 'romaji'})))
+		() => Promise.all(getWordsList('japanese', 10000).map(async w => kuroshiro.convert(w, { to: 'romaji'})))
 	)
 	.then(res => res.filter(w => !Kuroshiro.Util.hasKanji(w)))
 	.then(res => res.map(latinize))
-	.then(res => res.map(w => w.replace(/[^a-zA-Z0-9]/gi, '')))
+	.then(res => res.map(w => w.replace(/[^a-zA-Z0-9']/gi, '').toLowerCase()))
 	.then(res => res.filter(w => w.length >= 5))
-	.then(res => fs.writeFileSync('../data/japanese.txt', res.join('\n')))
+	.then(res => fs.writeFileSync('../data/common-words/japanese.txt', res.join('\n')))
 
 ;[
-	'polish', 'german', 'english',
-	'czech', 'slovak', 'italian',
-	'spanish', 'french', 'swedish'
+	'czech', 'dutch', 'english', 'finnish', 'german', 'italian', 
+	'norwegian', 'polish', 'portuguese', 'spanish', 'swedish'
 ].forEach(cat => {
 	fs.writeFileSync(
-		`../data/${cat}.txt`,
-		getWordsList(cat, 5000)
+		`../data/common-words/${cat}.txt`,
+		getWordsList(cat, 10000)
 			.map(latinize)
+			.map(w => w.replace(/[^a-zA-Z0-9']/gi, '').toLowerCase())
 			.filter(w => w.length >= 5)
 			.join('\n')
 	)
